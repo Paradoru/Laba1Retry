ed60b9d62386ea867745c26c4a484a6893a629f6
Paradoru <LicLexa@yandex.ru>
2022-06-13 20:23:45 +0200

    Добавьте файлы проекта.

---
+++ Laba1Retry/Silg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Lab1Retry
+{
+    internal class Silg
+    {
+        public double R { get; set; } // Скорость м/сек
+        public double T { get; set; } //Время в минутах
+        public Silg(double r, double t)
+        {
+            R = r;
+            T = t;
+        }
+        public double Rashet()
+        {
+            return R * 60 * T;
+        }
+    }
+
+}
